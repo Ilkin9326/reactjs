8e3fec94188a53492bfa662d33c3be300aed7bcb
Ilkin9326 <ilkin.isgenderov91@gmail.com>
2019-06-19 15:15:54 +0400

    Update test.jsx
    cavab geldi gitdden

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,3 +1,4 @@
 //salam Millet gorsen nece oldu emell egleidmi?
 //react js eladi ve gotde hemcijniden eladi
 //tekrar birde edek gorek ne olur?/*******************    2019      ********************/
+//birde burdan folder push edek.
