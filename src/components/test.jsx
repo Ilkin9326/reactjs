033eab2dd842ef0e9cccbd0dc1da1970f9ee0ce5
hikmetqurbanli <hikmetqurbanli@gmail.com>
2019-06-19 15:13:47 +0400

    tekrar banmdan geldi

--- src/components/test.jsx
+++ src/components/test.jsx
@@ -1,2 +1,3 @@
 //salam Millet gorsen nece oldu emell egleidmi?
 //react js eladi ve gotde hemcijniden eladi
+//tekrar birde edek gorek ne olur?/*******************    2019      ********************/
